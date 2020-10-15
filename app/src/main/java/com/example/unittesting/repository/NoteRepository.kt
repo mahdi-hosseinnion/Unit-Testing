a0591583d3d51024977ad2d2ea8725cae4246a31
seyed mahdi <ssmmhh1382@gmail.com>
2020-10-15 08:52:56 +0100

    19-Inserting data into room with RxJava

--- app/src/main/java/com/example/unittesting/repository/NoteRepository.kt
+++ app/src/main/java/com/example/unittesting/repository/NoteRepository.kt
@@ -1,6 +1,15 @@
 package com.example.unittesting.repository
 
+import com.example.unittesting.models.Note
 import com.example.unittesting.persistence.NoteDao
+import com.example.unittesting.ui.Resource
+import com.example.unittesting.util.Constants.NETWORK_DELAY
+import io.reactivex.Flowable
+import io.reactivex.Scheduler
+import io.reactivex.annotations.SchedulerSupport.IO
+import io.reactivex.schedulers.Schedulers
+import java.lang.Exception
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,7 +17,30 @@ import javax.inject.Singleton
 class NoteRepository
 @Inject
 constructor(
-    noteDao: NoteDao
+    val noteDao: NoteDao
 ) {
+
+
+    fun insertNote(note: Note): Flowable<Resource<Int>> = noteDao.insertNote(note)
+        .delaySubscription(NETWORK_DELAY, TimeUnit.MILLISECONDS)//for testing
+        //convert long to int b/c all other action return integer
+        .map { long ->
+            long.toInt()
+        }
+        .onErrorReturn { throwable ->
+            throwable.printStackTrace()
+            -1
+        }
+        .map { int ->
+            if (int > 0) {
+                //success
+                Resource.Success(int)
+            } else {
+                //error
+                Resource.Error("Inserting new note\n ERROR: Unknown error")
+            }
+        }
+        .subscribeOn(Schedulers.io())
+        .toFlowable()
 
 }
---
+++ app/src/main/java/com/example/unittesting/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.unittesting.util
+
+object Constants {
+
+    const val NETWORK_DELAY: Long = 0L
+}
